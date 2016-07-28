a08f7b7acaac2511ead98f7378770c72f07d4d42
JpnLavender <nagisa20000014@gmail.com>
2016-07-29 01:44:21 +0900

    [Delete] レコードがないときにslackに通知する奴

--- bot.rb
+++ bot.rb
@@ -35,6 +35,7 @@ puts "起動！"
 client.user do |tweet|
   case tweet
   when Twitter::Tweet
+    puts tweet.full_text 
     Curl.post("#{host}stocking_tweet", { tweet_id: tweet.id, user_name: tweet.user.screen_name, text: tweet.full_text})
     if tweet.text =~ /テスト/
       client_rest.favorite(tweet.id)
@@ -47,7 +48,8 @@ client.user do |tweet|
     if "#{tweet.id}" == JSON.parse(Curl.get("#{host}#{tweet.id}").body_str)["tweet_id"]
       slack_puts("Delete: #{tweet.name}-> #{tweet.text}")
     else 
-      slack_puts("誰かがつい消ししたっぽい")
+      # slack_puts("誰かがつい消ししたっぽい")
+      nil
     end
   end
 end
