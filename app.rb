f751d8b76e4acea2f01b38da5221c0b1417f2859
JpnLavender <nagisa20000014@gmail.com>
2016-07-28 20:58:37 +0900

    [Add] 404 Error -> Debug

--- app.rb
+++ app.rb
@@ -5,12 +5,14 @@ require './models.rb'
 get '/Lavender/:swich/:id' do
   case params[:swich]
   when "find_tweet"
-    tweet = Tweet.find_by(tweet_id: params[:id])
+    if tweet = Tweet.find_by(tweet_id: params[:id])
     {
       tweet_id: tweet.tweet_id,
       user_name: tweet.user_name,
       text: tweet.text
     }.to_json 
+    else
+      {error: "404"}.to_json 
     end
   end
 end
--- bot.rb
+++ bot.rb
@@ -35,14 +35,8 @@ puts "起動！"
 client.user do |tweet|
   case tweet
   when Twitter::Tweet
-    Curl.post(
-      "#{host}shtocking_tweet",
-      {
-        tweet_id: tweet.id, 
-        user_name: tweet.user.screen_name,
-        text: tweet.full_text,
-      }.to_json
-    )
+    p tweet.full_text 
+    Curl.post( "#{host}shtocking_tweet", { tweet_id: tweet.id, user_name: tweet.user.screen_name, text: tweet.full_text, }.to_json)
     if tweet.text =~ /テスト/
       client_rest.favorite(tweet.id)
       slack_puts(tweet)
