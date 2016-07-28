582b5baadeec6416496c253f19d7763469c97d6f
JpnLavender <nagisa20000014@gmail.com>
2016-07-28 15:37:57 +0900

    [Debug] send => slack_puts

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'twitter'
 require 'curb'
 
-def send(text)
+def slack_puts(text)
   Curl.post(ENV['WEBHOOKS'], {channel: "#bot_tech", username: "Lavender", text: text, icon_url: "http://i.imgur.com/Jjwsc.jpg"}.to_json)
 end
 
@@ -23,10 +23,10 @@ client.user do |object|
   if object.is_a?(Twitter::Tweet)
     if object.text =~ /テスト/
       client_rest.favorite(object.id)
-      send("[OK] Now Running Bot... ")
+      slack_puts("[OK] Now Running Bot... ")
     end
     if object.user.screen_name == "alpdaca"
-      send("@irimamekun  あるぱか→#{object.text}")
+      slack_puts("@irimamekun  あるぱか→#{object.text}")
     end
   end
 end
