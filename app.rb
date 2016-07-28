4b1213488d48e6c8269c2d650b528b5f625ce6c4
JpnLavender <nagisa20000014@gmail.com>
2016-07-29 00:00:24 +0900

    [Delete] 改行

--- app.rb
+++ app.rb
@@ -7,11 +7,7 @@ get '/Lavender/:swich/:id' do
   case params[:swich]
   when "find_tweet"
     if tweet = Tweet.find_by(tweet_id: params[:id])
-    {
-      tweet_id: tweet.tweet_id,
-      user_name: tweet.user_name,
-      text: tweet.text
-    }.to_json 
+    { tweet_id: tweet.tweet_id, user_name: tweet.user_name, text: tweet.text }.to_json 
     else
       {error: "404"}.to_json 
     end
