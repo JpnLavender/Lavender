814ddc4686612b35d54694b777296abd73088e10
JpnLavender <nagisa20000014@gmail.com>
2016-07-28 19:52:59 +0900

    [Change] app.rb

--- app.rb
+++ app.rb
@@ -1,54 +1,10 @@
-require 'twitter'
-require 'curb'
+require 'bundler/setup'
+Bundler.require
+require './models.rb'
 
-def slack_puts(tweet)
-  Curl.post(
-    ENV['WEBHOOKS'],
-    { 
-      channel: "#bot_tech",
-      username: "Lavender ",
-      text: tweet.full_text,
-      icon_url: "http://19.xmbs.jp/img_fget.php/_bopic_/923/e05cec.png"
-    }.to_json
-  )
-  puts "#{Time.now} 受信"
-end
-
-client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key    = ENV["CONSUMER_KEY"]
-  config.consumer_secret = ENV["CONSUMER_SECRET"]
-  config.access_token    = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-end
-
-client_rest = Twitter::REST::Client.new do |config|
-  config.consumer_key    = ENV["CONSUMER_KEY"]
-  config.consumer_secret = ENV["CONSUMER_SECRET"]
-  config.access_token    = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-end
-
-puts "起動！"
-
-client.user do |tweet|
-  Tweet.create(
-    tweet_id: tweet.id, 
-    user_name: tweet.user.screen_name,
-    text: tweet.full_text,
-    img_url: .media.first.expanded_url.to_s
-  )
-  case tweet
-  when Twitter::Tweet
-    if tweet.text =~ /テスト/
-      client_rest.favorite(tweet.id)
-      slack_puts(tweet)
-    end
-    if tweet.user.screen_name == "alpdaca"
-      slack_puts(tweet)
-    end
-  when Twitter::Streaming::DeletedTweet
-    if tweet = Tweet.find_by(tweet_id: tweet.id)
-      slack_puts("Delete: #{tweet.name}-> #{tweet.text}")
-    end
+get 'Lavender/:swich/:id' do
+  case params[:swich]
+  when find_tweet
+    p tweet = Tweet.find_by(tweet_id: params[:id])
   end
 end
