398b16ba4951b37147607c38f03fc876cce85957
Alpdaca <JpnLavender>
2016-07-30 15:55:06 +0000

    [Change] Delete Tweet をわかりやすく

--- bot.rb
+++ bot.rb
@@ -20,6 +20,7 @@ def option(tweet)
     attachments: [{
       author_icon: tweet.user.profile_image_url,
       author_name: tweet.user.name,
+      author_subname: "@#{tweet.user.screen_name}",
       text: tweet.full_text,
       image_url: tweet.media,
       author_link: tweet.uri,
@@ -33,7 +34,7 @@ def delete(tweet)
     attachments: [{
       author_icon: tweet[:icon],
       author_name: tweet[:user_name],
-      text: tweet[:text],
+      text: "Delete:\n#{tweet[:text]}",
       # image_url: tweet.media,
       author_link: tweet[:url],
       color: "red",
@@ -60,24 +61,16 @@ puts "起動！"
 client.user do |tweet|
   case tweet
   when Twitter::Tweet
-    puts "#{tweet.user.name} -> #{tweet.full_text}" 
+    puts "#{tweet.user.name} -> #{tweet.full_text}\n\n" 
     Curl.post("#{host}/stocking_tweet", { tweet_id: tweet.id, user_name: tweet.user.name, text: tweet.full_text, url:tweet.uri, icon: tweet.user.profile_image_url})
-    case tweet.text 
-    when /テスト/
-      client_rest.favorite(tweet.id)
-      slack_puts("正常に動いてますよ^^")
-    when /社畜ちゃん/
-      client_rest.favorite(tweet.id)
-      slack_puts("#{tweet.user.name}が呼んでるよ")
-    end
     case tweet.user.screen_name 
-    when "alpdaca"
+    when "alpdaca" , "ni_sosann" , "usr_meloco" , "serin_inaka", "osrmishi"
       option(tweet)
     end
   when Twitter::Streaming::DeletedTweet
     data = JSON.parse(Curl.get("#{host}/Lavender/find_tweet/#{tweet.id}").body_str)
     if "#{tweet.id}" == data["tweet_id"]
-      slack_puts(data)
+      delete(data)
     else 
       puts ("誰かがつい消ししたっぽい")
     end
