cdc18230055538ae93917cdc2ddaac41e06620fc
JpnLavender <nagisa20000014@gmail.com>
2016-07-31 18:58:58 +0900

    [AddSetting] get icon_url, url, media_url -> create

--- app.rb
+++ app.rb
@@ -19,5 +19,11 @@ get '/Lavender/:swich/:id' do
 end
 
 post '/stocking_tweet' do
-  Tweet.create(tweet_id: params["tweet_id"], user_name: params["user_name"], text: params["text"])
+  Tweet.create(tweet_id: params["tweet_id"],
+               user_name: params["user_name"],
+               text: params["text"],
+               icon: params["icon"],
+               url: params["url"],
+               media: params["media"]
+              )
 end
