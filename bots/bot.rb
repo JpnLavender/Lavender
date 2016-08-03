aa47b15ae53b2664df31c0ad003e149ffc5b2fd7
JpnLavender <nagisa20000014@gmail.com>
2016-08-03 19:08:24 +0900

    [Debug] Tweet -> Twitter

--- bots/bot.rb
+++ bots/bot.rb
@@ -1,17 +1,17 @@
 require 'twitter'
 require './models.rb'
 puts "起動！"
-p Tweet.config
-Tweet.config.user do |tweet|
+p t = Tweet.config
+t.user do |tweet|
   case tweet
-  when Tweet::Tweet
+  when Twitter::Tweet
     puts "#{tweet.user.name} -> #{tweet.full_text}\n\n" #テストコード
     Tweet.database_post(tweet)
     case tweet.user.screen_name 
     when "alpdaca" , "ni_sosann" , "usr_meloco" , "osrmishi"
       Tweet.slack_post_options(tweet)
     end
-  when Tweet::Streaming::DeletedTweet
+  when Twitter::Streaming::DeletedTweet
     data = JSON.parse(Curl.get("#{$host}/Lavender/find_tweet/#{tweet.id}").body_str)
     if "#{tweet.id}" == data["tweet_id"]
       Tweet.deleted_tweet(data)
