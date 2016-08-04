8481b1a652129f31d02e49c475f0cee45314a5e5
JpnLavender <nagisa20000014@gmail.com>
2016-08-04 13:41:58 +0900

    [AddSetting] tweet_checker

---
+++ bots/tweet_checker.rb
@@ -0,0 +1,12 @@
+puts "TweetCheckr"
+require './models.rb'
+
+Tweet.config.user do |tweet|
+  case tweet
+  when Twitter::Tweet 
+    case tweet
+    when /社畜ちゃん/
+      Slappy.say "#{tweet.user.name}に呼ばれてるよ！\n #{tweet.uri}"
+    end
+  end
+end
