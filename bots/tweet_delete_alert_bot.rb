19019ce7e374b4124ee0dca3d596a04521b8d258
JpnLavender <nagisa20000014@gmail.com>
2016-08-05 22:32:29 +0900

    [Add] Option Type

--- bots/tweet_delete_alert_bot.rb
+++ bots/tweet_delete_alert_bot.rb
@@ -1,7 +1,8 @@
 require 'twitter'
 require './models.rb'
 
-puts "ついけし監視Bot起動！"
+puts "ついけし監視 -> #{$deleted_streaming}"
+puts "ユーザー監視 -> #{$streaming}"
 
 Tweeted.config.user do |tweet|
   case tweet
@@ -14,7 +15,7 @@ Tweeted.config.user do |tweet|
   when Twitter::Streaming::DeletedTweet
     data = JSON.parse(Curl.get("#{$host}/Lavender/find_tweet/#{tweet.id}").body_str)
     if "#{tweet.id}" == data["tweet_id"]
-      $deleted_streaming ? Tweeted.slack_post_options(data) : "この機能は現在停止中です" 
+      $deleted_streaming ? Tweeted.slack_post_options(data.merge!({type: "delete"})) : "この機能は現在停止中です" 
     else 
       # Slappy.say "誰かがつい消ししたっぽい"
     end
