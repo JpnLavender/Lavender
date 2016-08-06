5340a184621515ff6d0e5fdfff40686870ebd47d
JpnLavender <nagisa20000014@gmail.com>
2016-08-06 11:55:23 +0900

    [Split] UserStreaming / DeleteStreaming

--- bots/tweet_checker.rb
+++ bots/tweet_checker.rb
@@ -1,12 +1,18 @@
 puts "TweetCheckr"
 require './models.rb'
 
+puts "ユーザー監視 -> #{$streaming}"
+
 Tweeted.config.user do |tweet|
   case tweet
   when Twitter::Tweet 
-    case tweet
+    case tweet.full_text 
     when /社畜ちゃん/
       Slappy.say "#{tweet.user.name}に呼ばれてるよ！\n #{tweet.uri}"
+    end
+    case tweet.user.screen_name 
+    when "alpdaca" , "ni_sosann" , "usr_meloco" , "osrmishi"
+      $streaming ? Tweeted.slack_post_options(tweet) : "この機能は現在停止中です" 
     end
   end
 end
--- bots/tweet_delete_alert_bot.rb
+++ bots/tweet_delete_alert_bot.rb
@@ -2,17 +2,10 @@ require 'twitter'
 require './models.rb'
 
 puts "ついけし監視 -> #{$deleted_streaming}"
-puts "ユーザー監視 -> #{$streaming}"
 
 Tweeted.config.user do |tweet|
   case tweet
-  when Twitter::Tweet
-    Tweeted.database_post(tweet)
-    case tweet.user.screen_name 
-    when "alpdaca" , "ni_sosann" , "usr_meloco" , "osrmishi"
-      $streaming ? Tweeted.slack_post_options(tweet) : "この機能は現在停止中です" 
-    end
-  when Twitter::Streaming::DeletedTweet
+    when Twitter::Streaming::DeletedTweet
     data = JSON.parse(Curl.get("#{$host}/Lavender/find_tweet/#{tweet.id}").body_str)
     if "#{tweet.id}" == data["tweet_id"]
       $deleted_streaming ? Tweeted.slack_post_options(data) : "この機能は現在停止中です" 
