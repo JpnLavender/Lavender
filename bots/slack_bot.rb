55bfe9123fd020dbe2228f1ddebbb1491260fbeb
JpnLavender <nagisa20000014@gmail.com>
2016-08-07 09:44:05 +0900

    [Change] BotSetting method : stop -> stop! & start -> start!

--- bots/slack_bot.rb
+++ bots/slack_bot.rb
@@ -14,26 +14,26 @@ end
 #===BotStop===
 hear /DeleteStreamingStop/i do |e|
   Slack.send("OK... ,Delete Streaming Stop Setting Now ,Wait Press") 
-  Tweet::DeletedStreaming.stop(e, "DeletedStreamingStop")
+  Tweet::DeletedStreaming.stop!(e, "DeletedStreamingStop")
   Slack.send("Setting All Complete, User Streaming Stop!!-> #{$deleted_streaming}")
 end
 
 hear /UserStreamingStop/i do |e|
   Slack.send(e, "OK... ,User Streaming Stop Setting Now ,Wait Press") 
-  Tweet::UserStreaming.stop("UserStreamingStop")
+  Tweet::UserStreaming.stop!("UserStreamingStop")
   Slack.send(e, "Setting All Complete, User Streaming Stop!!->#{$user_streaming}") 
 end
 
 #===BotStart===
 hear /DeleteStreamingStart/i do |e|
   Slack.send(e, "OK... ,Delete Streaming Start Setting Now ,Wait Press") 
-  Tweet::DeletedStreaming.start(e, "DeletedStreamingStart")
+  Tweet::DeletedStreaming.start!(e, "DeletedStreamingStart")
   Slack.send(e, "Setting All Complete, Delete Streaming Start!->#{$user_streaming}") 
 end
 
 hear /UserStreamingStart/i do |e|
   Slack.send(e, "OK... ,User Streaming Start Setting Now ,Wait Press") 
-  Tweet::UserStreaming.start("UserStreamingStart")
+  Tweet::UserStreaming.start!("UserStreamingStart")
   Slack.send(e,"Setting All Complete, User Streaming Start!->#{$user_streaming}") 
 end
 
