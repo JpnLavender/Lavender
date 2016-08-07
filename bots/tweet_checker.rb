208f923fb61a4191ef90ae164a7eac29162e9295
JpnLavender <nagisa20000014@gmail.com>
2016-08-07 16:10:33 +0900

    [Change] FavoUserSelectMehod -> use TwitterList

--- bots/tweet_checker.rb
+++ bots/tweet_checker.rb
@@ -2,6 +2,7 @@ puts "TweetCheckr"
 require './models.rb'
 
 puts "ユーザー監視 -> #{$user_streaming}"
+favo_user = Tweet.list_join_members(762160635719225344)
 
 Tweet.config.user do |tweet|
   case tweet
@@ -11,7 +12,7 @@ Tweet.config.user do |tweet|
       Slappy.say "#{tweet.user.name}に呼ばれてるよ！\n #{tweet.uri}"
     end
     case tweet.user.screen_name 
-    when "alpdaca" , "ni_sosann" , "usr_meloco" , "osrmishi"
+    when *favo_user 
       puts $user_streaming 
       $user_streaming ? Tweet.slack_post_options(tweet) : "この機能は現在停止中です" 
     end
