1e71420d979516d8e6ed2b8e248b00bdddaceefd
JpnLavender <nagisa20000014@gmail.com>
2016-08-09 18:46:30 +0900

    [merge] AyamiBot: if fork

--- bots/ayami_old.rb
+++ bots/ayami_old.rb
@@ -1,11 +1,8 @@
 require './models.rb'
 puts "Ayami_old_Bot"
 
-puts "AyamiBot -> #{$user_streaming}"
 Tweet.config.user do |tweet|
-  if tweet == Twitter::Tweet
-    if tweet.user.screen_name == 'ayamin_talk' && tweet.full_text =~ /彼/
-      Tweet.config_rest.update("かつみー" * 3)
-    end
+  if tweet == Twitter::Tweet && tweet.user.screen_name == 'ayamin_talk' && tweet.full_text =~ /彼/
+    Tweet.config_rest.update("かつみー" * 3)
   end
 end
