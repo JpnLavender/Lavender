35a43fe69618df4b3754c325746c600d4e84cc04
JpnLavender <nagisa20000014@gmail.com>
2016-08-25 11:54:41 +0900

    [Debug] == -> +~

--- bots/amahi_study_assignment_bot.rb
+++ bots/amahi_study_assignment_bot.rb
@@ -2,7 +2,10 @@ require './models'
 puts "study_assignment_bot"
 
 Tweet.config.user do |tweet|
-  if tweet.is_a?(Twitter::Tweet) && tweet.user.screen_name == /sawawankagi|h_ayaha19|hananigashi19|sawasan0519/ 
-    Tweet.config_rest.update("@#{object.user.screen_name} #{"課題" * 30}", options = {in_reply_to_status_id: object.id})
+  if tweet.class == Twitter::Tweet && tweet.user.screen_name =~ /sawawankagi|h_ayaha19|hananigashi19|sawasan0519/ 
+    puts "Success"
+    Tweet.config_rest.update("@#{tweet.user.screen_name} 課題しろ！！！", options = {in_reply_to_status_id: tweet.id})
+  else
+    puts "faaaaaa"
   end
 end
