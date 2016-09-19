2f00a5887ea010a6d5caea592200e5ca2ba02004
JpnLavender <nagisa20000014@gmail.com>
2016-09-19 21:49:18 +0900

    MicroChanged at DeleteChecker

--- TweetDeleteAlert.rb
+++ TweetDeleteAlert.rb
@@ -3,14 +3,13 @@ require 'curb'
 require 'hashie'
 require "active_record"
 require './models.rb'
-
 $host = ENV['HOST']
 
 class TweetDeleteChecker
 
   def initialize(config)
     @config = config
-    @rest = Twitter::REST::Client.new(@config)
+    @rest   = Twitter::REST::Client.new(@config)
     @stream = Twitter::Streaming::Client.new(@config)
   end
   attr_reader :config, :rest, :stream
@@ -25,21 +24,21 @@ class TweetDeleteChecker
 
   def slack_post(attachments)
     conf = { channel: "#bot_tech", username: "Lavender", icon_url: "http://19.xmbs.jp/img_fget.php/_bopic_/923/e05cec.png"}.merge(attachments)
-    Curl.post( ENV['WEBHOOKS'],JSON.pretty_generate(conf))
+    Curl.post( ENV['WEBHOOKS'], JSON.pretty_generate(conf) )
     puts JSON.pretty_generate(conf)
   end
 
   def slack_post_options(tweet)
     attachments = [{
-      author_icon: tweet.user.profile_image_url.to_s,
-      author_name: tweet.user.name,
+      author_icon:    tweet.user.profile_image_url.to_s,
+      author_name:    tweet.user.name,
       author_subname: "@#{tweet.user.screen_name}",
-      text: tweet.full_text,
-      author_link: tweet.uri.to_s,
-      color: tweet.user.profile_link_color
+      text:           tweet.full_text,
+      author_link:    tweet.uri.to_s,
+      color:          tweet.user.profile_link_color
     }] 
     if tweet.media
-      tweet.media.each_with_index do |v,i|
+      tweet.media.each_with_index do |v, i|
         attachments[i] ||= {}
         attachments[i].merge!({image_url: v.media_uri })
       end
@@ -49,27 +48,27 @@ class TweetDeleteChecker
 
   def database_post(tweet)
     Tweet.create( 
-                 tweet_id: tweet.id,
+                 tweet_id:    tweet.id,
                  screen_name: tweet.user.screen_name,
-                 user_name: tweet.user.name,
-                 text: tweet.full_text,
-                 icon: tweet.user.profile_image_url,
-                 url:tweet.uri, 
-                 color: tweet.user.profile_link_color
+                 user_name:   tweet.user.name,
+                 text:        tweet.full_text,
+                 icon:        tweet.user.profile_image_url,
+                 url:         tweet.uri, 
+                 color:       tweet.user.profile_link_color
                 )
   end
 
   def tweet_data(id)
     if tweet = Tweet.find_by(tweet_id: id)
       {
-        tweet_id: tweet.tweet_id,
-        full_text: tweet.text,
-        uri: tweet.url,
-        media: nil,
+        tweet_id:             tweet.tweet_id,
+        full_text:            tweet.text,
+        uri:                  tweet.url,
+        media:                nil,
         user: {
-          name: tweet.screen_name,
-          screen_name: tweet.user_name,
-          profile_image_url: tweet.icon,
+          name:               tweet.screen_name,
+          screen_name:        tweet.user_name,
+          profile_image_url:  tweet.icon,
           profile_link_color: tweet.color
         }
       }
@@ -82,21 +81,14 @@ class TweetDeleteChecker
     @stream.user do |tweet|
       if tweet.is_a?(Twitter::Tweet)
         database_post(tweet)
-        case tweet.user.screen_name
-        when *favo_user
-          unless tweet.full_text =~ /^RT/ 
-            slack_post_options(tweet)
-            # Tweet.config_rest.favorite(tweet.id)
-          end
-        end
+        next unless favo_user.include?(tweet.user.screen_name)
+        next if tweet.full_text =~ /^RT/ 
+        slack_post_options(tweet)
       elsif tweet.is_a?(Twitter::Streaming::DeletedTweet)
         data = Hashie::Mash.new(tweet_data(tweet.id))
-        if "#{tweet.id}" == data.tweet_id
-          data.full_text = "Delete\n" + "#{data.full_text}"
-          slack_post_options(data)
-        else
-          # Slappy.say "誰かがつい消ししたっぽい"
-        end
+        next unless "#{tweet.id}" == data.tweet_id
+        data.full_text = "Delete\n" + "#{data.full_text}"
+        slack_post_options(data)
       end
     end
   end
@@ -104,9 +96,9 @@ class TweetDeleteChecker
 end
 
 CONFIG = {
-  consumer_key: ENV["SUB_CONSUMER_KEY"],
-  consumer_secret: ENV["SUB_CONSUMER_SECRET"],
-  access_token: ENV["SUB_ACCESS_TOKEN"],
+  consumer_key:        ENV["SUB_CONSUMER_KEY"],
+  consumer_secret:     ENV["SUB_CONSUMER_SECRET"],
+  access_token:        ENV["SUB_ACCESS_TOKEN"],
   access_token_secret: ENV["SUB_ACCESS_TOKEN_SECRET"]
 }
 
