f21563b30f54f569008963170ca2594d52b8a65a
JpnLavender <nagisa20000014@gmail.com>
2016-09-19 16:35:23 +0900

    Delete SlackBot

--- bots/AutoBlockBot.rb
+++ bots/AutoBlockBot.rb
@@ -1,3 +1,49 @@
-require './models'
+require 'twitter'
 
-Tweet.config.
+class AutoBlockBot
+  def initialize(config)
+    @config = config
+    @rest = Twitter::REST::Client.new(@config)
+    @stream = Twitter::Streaming::Client.new(@config)
+    @my_id = rest.user.id.freeze
+    @events = [:follow, :unfollow].freeze
+  end
+  attr_reader :config, :rest, :stream, :my_id, :events
+
+  def run
+    event_stream
+  end
+
+  def follow_list
+    @rest.friend_ids
+  end
+
+  def user_block(id)
+    @rest.block(id)
+    puts "SuccessUserBlock!"
+  end
+
+  def event_stream
+    @stream.user do |event|
+      next unless event.is_a?(Twitter::Streaming::Event)
+      puts "1"
+      next unless @events.include?(event.name)
+      puts "2"
+      next if follow_list.include?(event.source.id)
+      puts "move function user_block"
+      user_block(event.source.id)
+    end
+  end
+
+end
+
+CONFIG = {
+  consumer_key: ENV["SUB_CONSUMER_KEY"],
+  consumer_secret: ENV["SUB_CONSUMER_SECRET"],
+  access_token: ENV["SUB_ACCESS_TOKEN"],
+  access_token_secret: ENV["SUB_ACCESS_TOKEN_SECRET"]
+}
+
+app = AutoBlockBot.new(CONFIG)
+app.run
+
