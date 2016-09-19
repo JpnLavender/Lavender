9af4877d29dbcfd0dfcaf454e8bf3b8a76a7448f
JpnLavender <nagisa20000014@gmail.com>
2016-09-19 17:14:41 +0900

    Update AyaminBot

--- bots/AyaminBot.rb
+++ bots/AyaminBot.rb
@@ -1,8 +1,37 @@
-require './models.rb'
-puts "Ayami_old_Bot"
+class AyaminBot
 
-Tweet.config.user do |tweet|
-  if tweet.class == Twitter::Tweet && tweet.user.screen_name == 'ayamin_talk' && tweet.full_text =~ /彼/
-    Tweet.config_rest.update("@#{object.user.screen_name} #{"かつみー" * 3}", options = {in_reply_to_status_id: object.id})
+  def initialize(config)
+    @config = config
+    @rest = Twitter::REST::Client.new(@config)
+    @stream = Twitter::Streaming::Client.new(@config)
   end
+  attr_reader :config, :rest, :stream
+
+  def run
+    streaming_run
+  end
+  
+  def verification(data)
+    data.user.screen_name == "ayamin_talk" && data.full_text =~ /彼/ ? true : false
+  end
+
+  def streaming_run
+    @stream.user do |tweet|
+      next unless tweet.is_a?(Twitter::Tweet)
+      next unless verification(tweet)
+      @rest.update("@#{object.user.screen_name} #{"かつみー" * 3}", options = {in_reply_to_status_id: object.id})
+    end
+  end
+
+
 end
+
+CONFIG = {
+  consumer_key: ENV["SUB_CONSUMER_KEY"],
+  consumer_secret: ENV["SUB_CONSUMER_SECRET"],
+  access_token: ENV["SUB_ACCESS_TOKEN"],
+  access_token_secret: ENV["SUB_ACCESS_TOKEN_SECRET"]
+}
+
+app = AyaminBot.new(CONFIG)
+app.run
